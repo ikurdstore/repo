6478eba1439663821f37631eaa35d205cfae0d0f
ikurd <62894887+ikurdstore@users.noreply.github.com>
2020-12-05 02:00:26 +0300

    f

---
+++ includes/nav.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>iKURD</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.1/css/all.css">
+</head>
+<body>
+
+<div  class=" bg-white text-left border p-2 m-3 radius-10 shadow-lg">
+  <a class="navbar-brand text-dark text-sm-center ml-3" href="index.php">
+    <img src="img/ikurd0.png" width="60" height="60">
+    iKurd
+</a>
+  <a href="cydia://url/https://cydia.saurik.com/api/share#?source=http://www.kurdrepo.cf/" target="_blank" class="btn btn-outline-danger float-right m-3">سۆرس <i class="fas fa-archive"></i></a>
+</div>
+
