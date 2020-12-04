1aef0bcd23602ac192efbf1589b75d859b5acfdc
ikurd <62894887+ikurdstore@users.noreply.github.com>
2020-12-05 01:58:16 +0300

    i

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php include('includes/nav.php');?>
+
+<div class="bg-white float-right text-left col-lg-4 p-1 mr-3 mt-3 border radius-10 shadow-lg">
+<a class="navbar-brand m-1" href="index.php">
+    <img src="img/ikurd0.png" width="50" height="50" class="ml-3">
+  </a>
+  <a class="mt-3 mr-3 float-right btn btn-outline-success" href="cydia://package/io.github.karwanbk.sbutils">زیادکردن <i class="fas fa-plus"></i></a>
+</div>
+
+<?php include('includes/footer.php');?>
